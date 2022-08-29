e596bfaec5b406210b6ebca81e2fcb136e141135
Bohdan Hoiko <litvipenko@gmail.com>
2022-08-29 20:59:43 +0300

    initial commit

--- js/gsap-settings.js
+++ js/gsap-settings.js
@@ -1,34 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
 // title
-gsap.to(".main-title__recipe", {
-    scrollTrigger: {
-        trigger: ".header",
-        scrub: 1,
-        start: "top top",
-        markers: true,
-    },
-    y:-20,
-    duration: 2,
-});
+// gsap.to(".main-title__recipe", {
+//     scrollTrigger: {
+//         trigger: ".header",
+//         scrub: 1,
+//         start: "top top",
+//         markers: true,
+//     },
+//     y:-20,
+//     duration: 2,
+// });
 
 gsap.to(".main-title__script", {
     scrollTrigger: {
         trigger: ".header",
         scrub: 1,
         start: "top top",
-        markers: true,
+        // markers: true,
     },
-    y:-50,
+    x:-50,
     duration: 2,
 });
 
 gsap.to(".main-title__title", {
     scrollTrigger: {
         trigger: ".header",
-        scrub: 2,
+        scrub: 1,
         start: "top top",
-        markers: true,
+        // markers: true,
     },
     x:150,
     duration: 2,
@@ -37,10 +37,34 @@ gsap.to(".main-title__title", {
 gsap.to(".main-title__title_two", {
     scrollTrigger: {
         trigger: ".header",
-        scrub: 2.5,
+        scrub: 1,
         start: "top top",
-        markers: true,
+        // markers: true,
     },
     x:-150,
+    duration: 2,
+});
+
+
+// run line
+gsap.to(".run-line__item-inner_one", {
+    scrollTrigger: {
+        trigger: ".run-line",
+        scrub: 1,
+        start: "-70vh bottom",
+        // markers: true,
+    },
+    x:-200,
+    duration: 2,
+});
+
+gsap.to(".run-line__item-inner_two", {
+    scrollTrigger: {
+        trigger: ".run-line",
+        scrub: 1,
+        start: "-70vh bottom",
+        // markers: true,
+    },
+    x:200,
     duration: 2,
 });
