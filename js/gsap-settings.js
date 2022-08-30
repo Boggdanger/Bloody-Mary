e5630cd08146bcb0f28e8dc5ceb4e33aa753f415
Bohdan Hoiko <litvipenko@gmail.com>
2022-08-30 21:38:02 +0300

    initial commit

--- js/gsap-settings.js
+++ js/gsap-settings.js
@@ -68,3 +68,46 @@ gsap.to(".run-line__item-inner_two", {
     x:200,
     duration: 2,
 });
+
+// bg body
+// gsap.to("body", {
+//     scrollTrigger: {
+//         trigger: ".ingredient-inner",
+//         scrub: 5.5,
+//         start: "top bottom",
+//         // end: "bottom -150",
+//         toggleClass: "body-bg-two",
+//         toggleActions: "none none none none",
+//         markers: true,
+//     },
+// });
+
+
+gsap.to(".ingredient-inner", {
+    scrollTrigger: {
+        trigger: ".ingredient-inner",
+        scrub: 5.5,
+        start: "top 400px",
+        // end: "bottom -150",
+        toggleClass: {targets: "body", className: "body-bg-two"},
+        toggleActions: "none none none none",
+        // markers: true,
+    },
+    duration: 2,
+});
+
+// работы
+let tl= gsap.timeline({
+    scrollTrigger: {
+        trigger: ".ingredient",
+        scrub: 1.3,
+        start: "-300 center",
+        end: "-100 center",
+        // markers: true,
+    },
+    
+});
+
+// tl.from(".ingredient", {scale: 0.3, rotation:25, autoAlpha: 0});
+
+tl.from(".ingredient", {opacity: 0.3, bottom: -200, autoAlpha: 0});
