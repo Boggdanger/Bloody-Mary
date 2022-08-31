a4b08f8979fddf4b5816ef408da77da1c7a94dbc
Bohdan Hoiko <litvipenko@gmail.com>
2022-08-31 22:10:19 +0300

    initial commit

--- js/gsap-settings.js
+++ js/gsap-settings.js
@@ -1,17 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// title
-// gsap.to(".main-title__recipe", {
-//     scrollTrigger: {
-//         trigger: ".header",
-//         scrub: 1,
-//         start: "top top",
-//         markers: true,
-//     },
-//     y:-20,
-//     duration: 2,
-// });
-
 gsap.to(".main-title__script", {
     scrollTrigger: {
         trigger: ".header",
@@ -56,6 +44,7 @@ gsap.to(".run-line__item-inner_one", {
     },
     x:-200,
     duration: 2,
+    ease: "none",
 });
 
 gsap.to(".run-line__item-inner_two", {
@@ -67,6 +56,7 @@ gsap.to(".run-line__item-inner_two", {
     },
     x:200,
     duration: 2,
+    ease: "none",
 });
 
 // bg body
@@ -83,31 +73,223 @@ gsap.to(".run-line__item-inner_two", {
 // });
 
 
-gsap.to(".ingredient-inner", {
+// gsap.to(".ingredient-inner", {
+//     scrollTrigger: {
+//         trigger: ".ingredient-inner",
+//         scrub: 5.5,
+//         start: "top 400px",
+//         // end: "bottom -150",
+//         toggleClass: {targets: "body", className: "body-bg-two"},
+//         toggleActions: "none none none none",
+//         // markers: true,
+//     },
+//     duration: 2,
+// });
+
+// ingridients
+const ing = gsap.timeline({
     scrollTrigger: {
-        trigger: ".ingredient-inner",
-        scrub: 5.5,
-        start: "top 400px",
-        // end: "bottom -150",
-        toggleClass: {targets: "body", className: "body-bg-two"},
-        toggleActions: "none none none none",
-        // markers: true,
-    },
-    duration: 2,
-});
+      trigger: ".ingredient-inner",
+      start: "-300 center",
+      end: "-100 center",
+      scrub: 1,
+    //   markers: true
+    }
+  });
+
+ing.to("body", {backgroundColor: "#FDDD87"})
+    .from(".ingredient", {opacity: 0.3, bottom: -200, autoAlpha: 0});
+
+
+
+
+    const ingImg = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".ingredient-inner",
+          start: "-10 center",
+          end: "150 center",
+          scrub: 1,
+        //   markers: true
+        }
+      });
+    
+    ingImg.from(".ingredient img", {scale: 0.3, opacity: 0.3, y: 200, autoAlpha: 0});
+        
+
+
+
+// // работы
+// let tl= gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".ingredient",
+//         scrub: 1.3,
+//         start: "-300 center",
+//         end: "-100 center",
+//         // markers: true,
+//     },
+    
+// });
+
+// // tl.from(".ingredient", {scale: 0.3, rotation:25, autoAlpha: 0});
+
+// tl.from(".ingredient", {opacity: 0.3, bottom: -200, autoAlpha: 0});
+
+
+
+
+// gsap.from(".recipe-list__item", {
+//     scrollTrigger: {
+//         trigger: ".recipe-list__item",
+//         scrub: 1,
+//         start: "center bottom",
+//         // markers: true,
+//     },
+//     width:-200,
+//     duration: 2,
+// });
+
+
 
 // работы
-let tl= gsap.timeline({
+// let rec= gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".recipe-list__item",
+//         scrub: 1.3,
+//         start: "-300 center",
+//         end: "-100 center",
+//         markers: true,
+//     },
+    
+// });
+
+// recipe
+const rec = gsap.timeline({
     scrollTrigger: {
-        trigger: ".ingredient",
-        scrub: 1.3,
-        start: "-300 center",
-        end: "-100 center",
+      trigger: ".recipe",
+      start: "top 100",
+      end: "top top",
+      scrub: true,
+    //   markers: true
+    }
+  });
+
+
+rec.to("body", {backgroundColor: "#D03F36"})
+    .to(".recipe-title__item", {color: "#F1C650"})
+    .to(".recipe-list__txt", {color: "#fff"})
+    .to(".recipe-list__step", {color: "#D03F36"})
+    .to(".recipe-list__step", {backgroundColor: "#F1C650"})
+    .to(".recipe-image__frame", {backgroundColor: "#F1C650"});
+
+
+// recipe title
+gsap.to(".recipe-title__wrapper", {
+    scrollTrigger: {
+        trigger: ".recipe-title",
+        scrub: 1,
+        start: "-100 bottom",
+        end: "bottom top",
         // markers: true,
     },
-    
+    x:-800,
+    duration: 2,
+    ease: "none",
 });
 
-// tl.from(".ingredient", {scale: 0.3, rotation:25, autoAlpha: 0});
+// recipe photo
+// gsap.to(".recipe-wrapper", {
+//     scrollTrigger: {
+//         trigger: ".recipe-wrapper",
+//         scrub: 1.15,
+//         start: "top top",
+//         end: "bottom bottom",
+//         pin: ".recipe-image__inner",
+//         pinSpacing: false,
+//     },
+// duration: 2,
 
-tl.from(".ingredient", {opacity: 0.3, bottom: -200, autoAlpha: 0});
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create
+let recImg = gsap.matchMedia();
+
+// add a media query. When it matches, the associated function will run
+recImg.add("(min-width: 991px)", () => {
+
+    gsap.to(".recipe-wrapper", {
+        scrollTrigger: {
+            trigger: ".recipe-wrapper",
+            scrub: 1.15,
+            start: "top top",
+            end: "bottom bottom",
+            pin: ".recipe-image__inner",
+            pinSpacing: false,
+        },
+    duration: 2,
+
+    });
+});
+
+recImg.add("(max-width: 991px)", () => {
+
+    gsap.to(".recipe-wrapper", {
+        scrollTrigger: {
+            trigger: ".recipe-wrapper",
+            scrub: 1.15,
+            start: "top top",
+            end: "bottom 500",
+            pin: ".recipe-image__inner",
+            pinSpacing: false,
+            // markers: true,
+        },
+    duration: 2,
+
+    });
+
+});
+
+recImg.add("(max-width: 768px)", () => {
+
+    gsap.to(".recipe-wrapper", {
+        scrollTrigger: {
+            trigger: ".recipe-wrapper",
+            scrub: 1.15,
+            start: "top top",
+            end: "bottom 500",
+            pin: ".recipe-image__inner",
+            pinSpacing: false,
+            // markers: true,
+        },
+    duration: 2,
+    });
+
+    gsap.to(".recipe-image__inner", {
+        scrollTrigger: {
+            trigger: ".recipe-wrapper",
+            scrub: true,
+            start: "top top",
+            end: "top -50",
+            markers: true,
+        },
+        opacity: 0.15,
+        ease: "none",
+    });
+
+    // gsap.to(".recipe-image__inner", {opacity: 0.3});
+
+});
+
