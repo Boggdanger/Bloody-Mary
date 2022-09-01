73ea81e89bca6e464acaa99cdd3d199106f1bdd7
Bohdan Hoiko <litvipenko@gmail.com>
2022-09-02 01:27:17 +0300

    initial commit

--- js/gsap-settings.js
+++ js/gsap-settings.js
@@ -1,5 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
-
+// hero title
 gsap.to(".main-title__script", {
     scrollTrigger: {
         trigger: ".header",
@@ -16,7 +16,6 @@ gsap.to(".main-title__title", {
         trigger: ".header",
         scrub: 1,
         start: "top top",
-        // markers: true,
     },
     x:150,
     duration: 2,
@@ -27,7 +26,6 @@ gsap.to(".main-title__title_two", {
         trigger: ".header",
         scrub: 1,
         start: "top top",
-        // markers: true,
     },
     x:-150,
     duration: 2,
@@ -40,7 +38,6 @@ gsap.to(".run-line__item-inner_one", {
         trigger: ".run-line",
         scrub: 1,
         start: "-70vh bottom",
-        // markers: true,
     },
     x:-200,
     duration: 2,
@@ -52,39 +49,12 @@ gsap.to(".run-line__item-inner_two", {
         trigger: ".run-line",
         scrub: 1,
         start: "-70vh bottom",
-        // markers: true,
     },
     x:200,
     duration: 2,
     ease: "none",
 });
 
-// bg body
-// gsap.to("body", {
-//     scrollTrigger: {
-//         trigger: ".ingredient-inner",
-//         scrub: 5.5,
-//         start: "top bottom",
-//         // end: "bottom -150",
-//         toggleClass: "body-bg-two",
-//         toggleActions: "none none none none",
-//         markers: true,
-//     },
-// });
-
-
-// gsap.to(".ingredient-inner", {
-//     scrollTrigger: {
-//         trigger: ".ingredient-inner",
-//         scrub: 5.5,
-//         start: "top 400px",
-//         // end: "bottom -150",
-//         toggleClass: {targets: "body", className: "body-bg-two"},
-//         toggleActions: "none none none none",
-//         // markers: true,
-//     },
-//     duration: 2,
-// });
 
 // ingridients
 const ing = gsap.timeline({
@@ -93,7 +63,6 @@ const ing = gsap.timeline({
       start: "-300 center",
       end: "-100 center",
       scrub: 1,
-    //   markers: true
     }
   });
 
@@ -101,66 +70,17 @@ ing.to("body", {backgroundColor: "#FDDD87"})
     .from(".ingredient", {opacity: 0.3, bottom: -200, autoAlpha: 0});
 
 
+// ingridients img
+const ingImg = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".ingredient-inner",
+        start: "-10 center",
+        end: "150 center",
+        scrub: 1,
+    }
+    });
 
-
-    const ingImg = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".ingredient-inner",
-          start: "-10 center",
-          end: "150 center",
-          scrub: 1,
-        //   markers: true
-        }
-      });
-    
-    ingImg.from(".ingredient img", {scale: 0.3, opacity: 0.3, y: 200, autoAlpha: 0});
-        
-
-
-
-// // работы
-// let tl= gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".ingredient",
-//         scrub: 1.3,
-//         start: "-300 center",
-//         end: "-100 center",
-//         // markers: true,
-//     },
-    
-// });
-
-// // tl.from(".ingredient", {scale: 0.3, rotation:25, autoAlpha: 0});
-
-// tl.from(".ingredient", {opacity: 0.3, bottom: -200, autoAlpha: 0});
-
-
-
-
-// gsap.from(".recipe-list__item", {
-//     scrollTrigger: {
-//         trigger: ".recipe-list__item",
-//         scrub: 1,
-//         start: "center bottom",
-//         // markers: true,
-//     },
-//     width:-200,
-//     duration: 2,
-// });
-
-
-
-// работы
-// let rec= gsap.timeline({
-//     scrollTrigger: {
-//         trigger: ".recipe-list__item",
-//         scrub: 1.3,
-//         start: "-300 center",
-//         end: "-100 center",
-//         markers: true,
-//     },
-    
-// });
+ingImg.from(".ingredient img", {scale: 0.3, opacity: 0.3, y: 200, autoAlpha: 0});
 
 // recipe
 const rec = gsap.timeline({
@@ -169,10 +89,8 @@ const rec = gsap.timeline({
       start: "top 100",
       end: "top top",
       scrub: true,
-    //   markers: true
     }
   });
-
 
 rec.to("body", {backgroundColor: "#D03F36"})
     .to(".recipe-title__item", {color: "#F1C650"})
@@ -189,7 +107,6 @@ gsap.to(".recipe-title__wrapper", {
         scrub: 1,
         start: "-100 bottom",
         end: "bottom top",
-        // markers: true,
     },
     x:-800,
     duration: 2,
@@ -197,56 +114,12 @@ gsap.to(".recipe-title__wrapper", {
 });
 
 
-// recipe 1111
-const about = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".about",
-      start: "-100 center",
-      end: "250 center",
-      scrub: true,
-    //   markers: true
-    }
-  });
-
-  about.from(".about-txt__text", {opacity: 0.3, y: 200, autoAlpha: 0})
-       .from(".about-txt img", {scale:0.3, opacity: 0.3, y: 100, autoAlpha: 0});
-
-
-
-// recipe photo
-// gsap.to(".recipe-wrapper", {
-//     scrollTrigger: {
-//         trigger: ".recipe-wrapper",
-//         scrub: 1.15,
-//         start: "top top",
-//         end: "bottom bottom",
-//         pin: ".recipe-image__inner",
-//         pinSpacing: false,
-//     },
-// duration: 2,
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // create
 let recImg = gsap.matchMedia();
 
 // add a media query. When it matches, the associated function will run
 recImg.add("(min-width: 991px)", () => {
-
+    // pin image recipe
     gsap.to(".recipe-wrapper", {
         scrollTrigger: {
             trigger: ".recipe-wrapper",
@@ -271,7 +144,6 @@ recImg.add("(max-width: 991px)", () => {
             end: "bottom 500",
             pin: ".recipe-image__inner",
             pinSpacing: false,
-            // markers: true,
         },
     duration: 2,
 
@@ -289,7 +161,6 @@ recImg.add("(max-width: 768px)", () => {
             end: "bottom 500",
             pin: ".recipe-image__inner",
             pinSpacing: false,
-            // markers: true,
         },
     duration: 2,
     });
@@ -300,7 +171,6 @@ recImg.add("(max-width: 768px)", () => {
             scrub: true,
             start: "top top",
             end: "top -50",
-            // markers: true,
         },
         opacity: 0.15,
         ease: "none",
@@ -310,13 +180,26 @@ recImg.add("(max-width: 768px)", () => {
 });
 
 
+// about section
+const about = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".about",
+      start: "-100 center",
+      end: "250 center",
+      scrub: true,
+    }
+  });
+
+  about.from(".about-txt__text", {opacity: 0.3, y: 200, autoAlpha: 0})
+       .from(".about-txt img", {scale:0.3, opacity: 0.3, y: 100, autoAlpha: 0});
+
+
 // run line bottom
 gsap.to(".run-line__bottom-one", {
     scrollTrigger: {
         trigger: ".run-line_bottom",
         scrub: 1,
         start: "-70vh bottom",
-        // markers: true,
     },
     x:-200,
     duration: 2,
@@ -328,24 +211,21 @@ gsap.to(".run-line__bottom-two", {
         trigger: ".run-line_bottom",
         scrub: 1,
         start: "-70vh bottom",
-        // markers: true,
     },
     x:200,
     duration: 2,
     ease: "none",
 });
 
-// recipe
+// logotypes
 const logoBot = gsap.timeline({
     scrollTrigger: {
       trigger: ".logotypes",
       start: "top 600",
       end: "top 450",
       scrub: 4,
-    //   markers: true,
     }
-  });
-
+});
 
 logoBot.from(".logos__item:nth-child(1)", {y: 150, opacity: 0.2, deley: 0.05})
         .from(".logos__item:nth-child(2)", {y: 150, opacity: 0.2, deley: 0.1})
@@ -353,4 +233,17 @@ logoBot.from(".logos__item:nth-child(1)", {y: 150, opacity: 0.2, deley: 0.05})
         .from(".logos__item:nth-child(4)", {y: 150, opacity: 0.2, deley: 0.2})
         .from(".logos__item:nth-child(5)", {y: 150, opacity: 0.2, deley: 0.25})
         .from(".logos__item:nth-child(6)", {y: 150, opacity: 0.2, deley: 0.3})
-        .from(".logos__item:nth-child(7)", {y: 150, opacity: 0.2, deley: 0.35})
+        .from(".logos__item:nth-child(7)", {y: 150, opacity: 0.2, deley: 0.35});
+
+
+// footer
+const foot = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".footer",
+      start: "top bottom",
+      end: "top 450",
+      scrub: 4,
+    }
+});
+
+foot.from(".footer-title", {y: 150, opacity: 0.2});
--- js/main.js
+++ js/main.js
@@ -1,2 +1,52 @@
+// parallax
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+// scroll top
+function scrollTo(element) {
+    window.scroll({
+      left: 0, 
+      top: element.offsetTop, 
+      behavior: 'smooth'
+    })
+  }
+  
+  var button = document.querySelector('.footer-bot__btn');
+  var footer = document.querySelector('#header');
+  
+  button.addEventListener('click', () => {
+    scrollTo(footer);
+  });
+
+  //  custom cursor
+  var cursorinner = document.querySelector('.cursor2');
+  var ab = document.querySelectorAll('a, button');
+
+  document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+  });
+
+  document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursorinner.style.left = x + 'px';
+    cursorinner.style.top = y + 'px';
+  });
+
+  document.addEventListener('mousedown', function(){
+    cursorinner.classList.add('cursorinnerhover')
+  });
+
+  document.addEventListener('mouseup', function(){
+    cursorinner.classList.remove('cursorinnerhover')
+  });
+
+  ab.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      $('.cursor2').hide();
+    });
+    item.addEventListener('mouseleave', () => {
+      $('.cursor2').fadeIn();
+    });
+  });
